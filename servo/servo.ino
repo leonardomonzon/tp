49bdd547d83a36af44fac79f3d2486d69ffc26b6
leonardomonzon <136333667+leonardomonzon@users.noreply.github.com>
2023-08-15 09:38:52 -0300

    Add files via upload

---
+++ servo/servo.ino
@@ -0,0 +1,76 @@
+
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN  150 
+#define SERVOMAX  600 
+#define USMIN  600 
+#define USMAX  2400 
+#define SERVO_FREQ 50 
+
+uint8_t servonum = 0;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8 channel Servo test!");
+
+  pwm.begin();
+ 
+  pwm.setOscillatorFrequency(27000000);
+  pwm.setPWMFreq(SERVO_FREQ); 
+
+  delay(10);
+}
+
+
+
+
+void setServoPulse(uint8_t n, double pulse) {
+  double pulselength;
+  
+  pulselength = 1000000;  
+  pulselength /= SERVO_FREQ; 
+  Serial.print(pulselength); Serial.println(" us per period"); 
+  pulselength /= 4096; 
+  Serial.print(pulselength); Serial.println(" us per bit"); 
+  pulse *= 1000000;  
+  pulse /= pulselength;
+  Serial.println(pulse);
+  pwm.setPWM(n, 0, pulse);
+}
+
+
+
+
+void loop() {
+  
+  Serial.println(servonum);
+  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++) {
+    pwm.setPWM(servonum, 0, pulselen);
+  }
+
+  delay(500);
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--) {
+    pwm.setPWM(servonum, 0, pulselen);
+  }
+
+  delay(500);
+
+  
+  
+  for (uint16_t microsec = USMIN; microsec < USMAX; microsec++) {
+    pwm.writeMicroseconds(servonum, microsec);
+  }
+
+  delay(500);
+  for (uint16_t microsec = USMAX; microsec > USMIN; microsec--) {
+    pwm.writeMicroseconds(servonum, microsec);
+  }
+
+  delay(500);
+
+  servonum++;
+  if (servonum > 7) servonum = 0;
+}
